bdba4c51c60c6c13cb8cca22fea91d9b693f6d20
Nguyen Duy Thuan <nguyenduythuan@38tr.local>
2026-05-15 23:01:16 +0700

    db

--- BackEnd/Migrations/20260515143822_fix.Designer.cs
+++ BackEnd/Migrations/20260515143822_fix.Designer.cs
@@ -4,6 +4,7 @@ using Backend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BackEnd.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260515143822_fix")]
+    partial class fix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -975,9 +978,14 @@ namespace BackEnd.Migrations
                     b.Property<Guid?>("UsedBy")
                         .HasColumnType("char(36)");
 
+                    b.Property<Guid?>("UserID")
+                        .HasColumnType("char(36)");
+
                     b.HasKey("TicketID");
 
                     b.HasIndex("UsedBy");
+
+                    b.HasIndex("UserID");
 
                     b.ToTable("Ticket");
                 });
@@ -1569,9 +1577,15 @@ namespace BackEnd.Migrations
 
             modelBuilder.Entity("Backend.Models.Ticket", b =>
                 {
-                    b.HasOne("Backend.Models.User", "User")
+                    b.HasOne("Backend.Models.User", "UsedByUser")
                         .WithMany()
                         .HasForeignKey("UsedBy");
+
+                    b.HasOne("Backend.Models.User", "User")
+                        .WithMany()
+                        .HasForeignKey("UserID");
+
+                    b.Navigation("UsedByUser");
 
                     b.Navigation("User");
                 });
